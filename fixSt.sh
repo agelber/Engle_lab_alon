fca1cf1cad8fd9364dc804c693d2d2595b767d12
agelber <alon.gelber@childrens.harvard.edu>
2017-04-14 10:41:31 -0400

    Create fixSt.sh

---
+++ fixSt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                     
+
+f=$1
+sort $f | uniq -d | grep "@"  > temp.txt
+
+    while read in; do
+        rname=$in
+        i=1
+        echo $rname
+        while grep -q "$rname" $f; do
+            replace=${rname/0:0/$i:0}
+            sed -i.bu "0,/$rname/s/$rname/$replace/" "$f"
+            i=$i+1
+        done
+
+    done < temp.txt
