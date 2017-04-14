4ea5b44f92d9eb0c7bfe608b132a713825c60496
agelber <alon.gelber@childrens.harvard.edu>
2017-04-14 10:42:16 -0400

    Update fixSt.sh

--- fixSt.sh
+++ fixSt.sh
@@ -14,3 +14,27 @@ sort $f | uniq -d | grep "@"  > temp.txt
         done
 
     done < temp.txt
+
+
+#!/bin/bash                                                                     
+
+fdir=$1"/*"
+
+for f in $fdir
+do
+    sort $f | uniq -d | grep "@"  > temp.txt
+
+    while read in; do
+        rname=$in
+        i=1
+        rname=${rname%" "*}
+        while grep -q "$rname" $f; do
+            replace=${rname/0:0/$i:0}
+            sed '0,/$rname/{s/$rname/$replace/}' $f
+            i=$i+1
+        done
+
+    done < temp.txt
+
+
+done
