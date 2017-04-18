2e0d6edd0eca281a04ed25ac095beb4d8ee88b7a
agelber <alon.gelber@childrens.harvard.edu>
2017-04-18 14:59:33 -0400

    Update fixSt.sh

--- fixSt.sh
+++ fixSt.sh
@@ -1,5 +1,4 @@
-#!/bin/bash                                                                     
-#!/bin/bash                                                                     
+#!/bin/bash                                                                                                                                      
 
 fdir=$1"*"
 
